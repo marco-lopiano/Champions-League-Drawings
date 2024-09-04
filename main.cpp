1dfd579ed46c1560a7f09a0e7628f22b7f0936be
marco-lopiano <marco.lopiano.95@gmail.com>
2024-09-04 19:20:11 +0200

    can load json file and unpack it ad vector array of strings and randomly choose one element

--- main.cpp
+++ main.cpp
@@ -2,26 +2,32 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include "json.hpp" // super ugly
+#include <fstream> 
+
+// compiling with: g++ -o main -std=c++17 -Wall -Wextra -pedantic main.cpp
 
 using namespace std;
-
+using json = nlohmann::json;
 
 string selectRandom(vector<string>* array){
     vector<string> out;
     sample(
         (*array).begin(),
         (*array).end(),
-        back_inserter(out),
+        back_inserter(out), // seems unnecessary, here for the time being as it is working
         1,
         mt19937{random_device{}()}
     );
     return out[0];
 }
 
-
 int main() {
 
-    vector<string> myArray{ "Blue", "Red", "Orange" };
+    ifstream f("teams.json");
+    json teams = json::parse(f);
+
+    vector<string> myArray = teams["Pot1"].get<vector<string>>();
 
     string t = selectRandom( &myArray );
     cout << t << endl;
