9d74c6f5976aef210ed5e6f32b13b806f8ba5f9a
marco-lopiano <marco.lopiano.95@gmail.com>
2024-11-04 12:36:55 -0500

    added classes to handle per team pot drawings, working with one single team so I need to encapsulate it into a loop for all the teams. I need also to implement some logic for making sure that team of the same nationality are not put together

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <nlohmann/json.hpp>
 #include <fstream>
-#include <unordered_map>
 
 /*
 COMPILER COMMAND:
@@ -14,10 +13,30 @@ g++ -o main -std=c++17 -Wall -Wextra -I /opt/homebrew/Cellar/nlohmann-json/3.11.
 using namespace std;
 using json = nlohmann::json;
 
+class SinglePotDraw {
+    public:
+        string potType;
+        string home;
+        string away;
+        SinglePotDraw(vector<string> x, string y) {
+            home = x[0];
+            away = x[1];
+            potType = y;
+        }
+};
+
+class PerTeamCollectionOfDrawings {
+    public:
+        string team;
+        vector<SinglePotDraw> teamDrawings;
+        PerTeamCollectionOfDrawings(string name) {
+            team = name;
+        } 
+};
+
 vector<string> selectRandom(vector<string>* array, string avoid){
     vector<string> out;
-    // barely understanding what's going on here, will need to come back to this
-    // gets 3 random items from vector
+    // gets 3 random teams from vector
     sample(
         (*array).begin(),
         (*array).end(),
@@ -28,10 +47,10 @@ vector<string> selectRandom(vector<string>* array, string avoid){
 
     // If element is found found, erase it otherwise remove latest element 
     // to make sure we have only 2 entries
-    auto it = std::find(out.begin(), out.end(), avoid);
+    auto foundSelf = std::find(out.begin(), out.end(), avoid);
 
-    if (it != out.end()) {
-        out.erase(it);
+    if (foundSelf != out.end()) {
+        out.erase(foundSelf);
     } else {
         out.pop_back();
     }
@@ -39,43 +58,42 @@ vector<string> selectRandom(vector<string>* array, string avoid){
     return out;
 }
 
+void diplayTeamDrawings (PerTeamCollectionOfDrawings dataIn) {
+    cout << dataIn.team << endl;
+    cout << right << "--- H:   "
+        << setw(dataIn.teamDrawings[0].away.size()) << right << dataIn.teamDrawings[0].home << "   "
+        << setw(dataIn.teamDrawings[1].away.size()) << right << dataIn.teamDrawings[1].home << "   "
+        << setw(dataIn.teamDrawings[2].away.size()) << right << dataIn.teamDrawings[2].home << "   "
+        << setw(dataIn.teamDrawings[2].away.size()) << right << dataIn.teamDrawings[3].home << "   "
+        << endl;
+
+    cout << right << "--- A:   "
+        << setw(dataIn.teamDrawings[0].home.size()) << right << dataIn.teamDrawings[0].away << "   "
+        << setw(dataIn.teamDrawings[1].home.size()) << right << dataIn.teamDrawings[1].away << "   "
+        << setw(dataIn.teamDrawings[2].home.size()) << right << dataIn.teamDrawings[2].away << "   "
+        << setw(dataIn.teamDrawings[3].home.size()) << right << dataIn.teamDrawings[3].away << "   "
+        << endl;
+}
+
 int main() {
 
     ifstream f("teams.json");
     json teams = json::parse(f);
-    unordered_map<string, vector<string>> drawings;
-
+  
     vector<string> pots = {"Pot1", "Pot2", "Pot3", "Pot4"}; // TODO: get these from json file too
-    
+
     string currentTeam = "Internazionale";
-    vector<string> currentTeams = teams["Pot1"].get<vector<string>>();
+    PerTeamCollectionOfDrawings otherTest(currentTeam);
 
-    vector<string> test = selectRandom( &currentTeams, currentTeam);
+    for (auto p : pots){
+        vector<string> potTeams = teams[p].get<vector<string>>();
+        vector<string> potSelection = selectRandom( &potTeams, currentTeam );
 
-    drawings[currentTeam] = {test};
-
-
-    for (auto  i : drawings) {
-        for (auto j : i.second) {
-            cout << j << endl;
-        }
+        SinglePotDraw potDraw(potSelection, p);
+        otherTest.teamDrawings.push_back(potDraw);
     }
 
-
-
-    // cout << test << endl;
-    // cout << static_cast<int16_t>(res) << endl;
-
-    // for (auto p : pots){
-
-    //     vector<string> currentTeam = teams[p].get<vector<string>>();
-    //     for (auto t : currentTeam){
-    //         cout << t << endl;
-    //     }
-
-    //     string t = selectRandom( &currentTeam );
-    //     cout << "---" << t << endl;
-    // }
+    diplayTeamDrawings( otherTest );
     return 0;
 }
 
